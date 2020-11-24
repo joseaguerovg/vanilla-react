acec0ee878ef999d64af4f657a5e2f04e3800fa0
joseaguerovg <joseaguero.vg@gmail.com>
2020-11-24 18:38:37 -0300

    Renderizando componentes

---
+++ components/app.js
@@ -0,0 +1,19 @@
+import {Component} from '../lib/react.js'
+import User from './user.js'
+import Wrapper from './wrapper.js'
+
+class App extends Component {
+    render() {
+        return `
+            <div class="app">
+                ${new Wrapper({
+                    children: `
+                        ${new User({name: 'Ash', avatar: './images/ash.jpg'}).render()}
+                    `
+                }).render()}
+            </div>
+        `
+    }
+}
+
+export default App
--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-import User from './components/user.js'
+import App from './components/app.js'
 import { render } from './lib/react-dom.js'
 
-render(new User({
-    avatar: './images/ash.jpg',
-    name: 'Ash'
-}), document.getElementById('root'))
+render(new App(), document.getElementById('root'))
