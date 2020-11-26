8f69d914a0b7919a49a5d05b9c22374dd227a7be
joseaguerovg <joseaguero.vg@gmail.com>
2020-11-26 12:36:12 -0300

    mejorando createElement

--- components/app.js
+++ components/app.js
@@ -3,20 +3,16 @@ import {Component, createElement} from '../lib/react/index.js'
 // import Wrapper from './wrapper.js'
 // import UserStyled from './user-styled.js'
 
-const element = createElement('h1', {
-    class: 'title'
-}, 'Hola mundo')
-
-console.log(element)
-
 class App extends Component {
     render() {
-        return `
-            <div class="app">
-                <h1>Hola</h1>
-                </div>`
-            }
-        }
+        return createElement('div', {
+            class: 'app',
+            children: [
+                createElement('h1', { class: 'title' }, 'Hola')
+            ]
+        }, 'Esta es la app')
+    }
+}
 // ${new Wrapper({
 //     children: `
 //         ${new User({name: 'Ash', avatar: './images/ash.jpg'}).render()}
