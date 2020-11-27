8d66ae09087ee0c8d8102ca6d502f35328ea2240
joseaguerovg <joseaguero.vg@gmail.com>
2020-11-27 00:30:22 -0300

    mejorando styled-component

--- components/app.js
+++ components/app.js
@@ -1,7 +1,7 @@
 import {Component, createElement} from '../lib/react/index.js'
 import User from './user.js'
 import Wrapper from './wrapper.js'
-// import UserStyled from './user-styled.js'
+import UserStyled from './user-styled.js'
 
 class App extends Component {
     render() {
@@ -11,6 +11,11 @@ class App extends Component {
                 new Wrapper({
                     children: [
                         new User({
+                            name: 'Ash',
+                            avatar: './images/ash.jpg',
+                            age: 10
+                        }),
+                        new UserStyled({
                             name: 'Ash',
                             avatar: './images/ash.jpg',
                             age: 10
